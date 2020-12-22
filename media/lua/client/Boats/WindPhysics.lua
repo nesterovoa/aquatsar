07bdf9de5e7b00f8b20b058e25cc34ba1d1ef981
Alexander Blinov <sashablinov1998@gmail.com>
2020-12-22 21:50:54 +0500

    big step

--- media/lua/client/Boats/WindPhysics.lua
+++ media/lua/client/Boats/WindPhysics.lua
@@ -3,6 +3,9 @@ if WindPhysics == nil then WindPhysics = {} end
 local vec1 = Vector3f.new()
 local vec2 = Vector3f.new()
 
+local windForce = Vector3f.new()
+local sailVector = Vector3f.new()
+
 function WindPhysics.getWindDirection()
     return ClimateManager.getWindAngleString(getClimateManager():getWindAngleDegrees())
 end
@@ -22,22 +25,87 @@ function WindPhysics.updateVehicles()
                 startCoeff = 5
             end
             
-            print(WindPhysics.getWindDirection())
+            
 
             local angle = math.rad(math.fmod(getClimateManager():getWindAngleDegrees(), 360))
             local x = math.cos(angle)
             local y = math.sin(angle)
+            windForce:set(x, y, 0)
 
-            local forceVector = vec1:set(x, y, 0)
-            forceVector:mul(335 * WindPhysics.getWindSpeed() * AquaBoats[vehicle:getScript():getName()].windInfluence * startCoeff)
+            print("Direction ", WindPhysics.getWindDirection())
+            print("Wind angle ", math.fmod(getClimateManager():getWindAngleDegrees(), 360))
+            print("Wind force ", windForce)
+
+            local sailAngle = vehicle:getModData()["sailAngle"]
+            if sailAngle == nil then
+                sailAngle = 0
+            end
+
+            
+
+            x = math.cos(math.rad(sailAngle))
+            y = math.sin(math.rad(sailAngle))
+
+            sailVector:set(x, y, 0)
+
+            print("Sail angle ", sailAngle)
+            print("Sail vector ", sailVector)
+
+
+            -- вот тут взять относительные координаты паруса!
+
+
+            -- проекция
+
+            local value = windForce:x() * sailVector:x() + windForce:y()*sailVector:y()
+
+            print("Value ", value)
+
+            print(math.abs(335 * WindPhysics.getWindSpeed() * value * AquaBoats[vehicle:getScript():getName()].windInfluence * startCoeff))
+
+            local forceVector = vehicle:getWorldPos(0, 0, 1, vec1):add(-vehicle:getX(), -vehicle:getY(), -vehicle:getZ())
+            forceVector:mul(math.abs(335 * WindPhysics.getWindSpeed() * value * AquaBoats[vehicle:getScript():getName()].windInfluence * startCoeff))
             forceVector:set(forceVector:x(), forceVector:z(), forceVector:y())
 
             vehicle:setPhysicsActive(true)
             vec2:set(0, 0, 0)
-            vehicle:addImpulse(forceVector, vec2)            
+            vehicle:addImpulse(forceVector, vec2)   
+            
+            if vehicle:getDriver() then
+                if isKeyDown(Keyboard.KEY_A) then
+                    vehicle:update()
+                        forceVector = vehicle:getWorldPos(1, 0, 0, vec1):add(-vehicle:getX(), -vehicle:getY(), -vehicle:getZ())
+                        forceVector:mul(600 * WindPhysics.getWindSpeed() * value * AquaBoats[vehicle:getScript():getName()].windInfluence * startCoeff)
+                        forceVector:set(forceVector:x(), forceVector:z(), forceVector:y())
+                        
+                        vehicle:getWorldPos(0, 0, -3, vec2):add(-vehicle:getX(), -vehicle:getY(), -vehicle:getZ())
+                        vec2:set(vec2:x(), vec2:z(), vec2:y())
+                        vehicle:addImpulse(forceVector, vec2)   
+                        print("Force ", forceVector)
+                        print(vec2)
+                elseif isKeyDown(Keyboard.KEY_D) then
+                    vehicle:update()
+                    forceVector = vehicle:getWorldPos(-1, 0, 0, vec1):add(-vehicle:getX(), -vehicle:getY(), -vehicle:getZ())
+                    forceVector:mul(600 * WindPhysics.getWindSpeed() * value * AquaBoats[vehicle:getScript():getName()].windInfluence * startCoeff)
+                    forceVector:set(forceVector:x(), forceVector:z(), forceVector:y())
+                    
+                    vehicle:getWorldPos(0, 0, -3, vec2):add(-vehicle:getX(), -vehicle:getY(), -vehicle:getZ())
+                    vec2:set(vec2:x(), vec2:z(), vec2:y())
+                    vehicle:addImpulse(forceVector, vec2)  
+                elseif isKeyDown(Keyboard.KEY_B) then
+                    if sailAngle < 180 then
+                        sailAngle = sailAngle + 1
+                    end
+                    vehicle:getModData()["sailAngle"] = sailAngle
+                elseif isKeyDown(Keyboard.KEY_N) then
+                    if sailAngle > 0 then
+                        sailAngle = sailAngle - 1
+                    end
+                    vehicle:getModData()["sailAngle"] = sailAngle
+                end
+            end
         end
     end	
 end
-
 
 Events.OnTick.Add(WindPhysics.updateVehicles)
