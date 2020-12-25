3e8f4a23c87cca8d27891a1b257ae71948fe012b
Alexander Blinov <sashablinov1998@gmail.com>
2020-12-26 00:20:16 +0500

    Drop items to water

---
+++ media/lua/client/Boats/TimedActions/ISDropItemToWaterAction.lua
@@ -0,0 +1,41 @@
+require "TimedActions/ISBaseTimedAction"
+
+ISDropItemToWaterAction = ISBaseTimedAction:derive("ISDropItemToWaterAction")
+
+function ISDropItemToWaterAction:isValid()
+	return true
+end
+
+function ISDropItemToWaterAction:update()
+	
+end
+
+function ISDropItemToWaterAction:start()
+	self:setActionAnim("Loot")
+end
+
+function ISDropItemToWaterAction:stop()
+	ISBaseTimedAction.stop(self)
+end
+
+function ISDropItemToWaterAction:perform()
+	if self.item:getContainer() ~= nil then
+		self.item:getContainer():Remove(self.item)
+	end
+	ISBaseTimedAction.perform(self)
+end
+
+function ISDropItemToWaterAction:new(character, item)
+	local o = {}
+	setmetatable(o, self)
+	self.__index = self
+	o.character = character
+	o.item = item
+	o.maxTime = 100
+	
+	return o
+end
+
+
+
+
