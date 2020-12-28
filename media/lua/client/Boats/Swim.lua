0f07d5b622ba3ce8653badb3ca9d20e31b5f49c1
Alexander Blinov <sashablinov1998@gmail.com>
2020-12-28 19:33:53 +0500

    New swim click context

--- media/lua/client/Boats/Swim.lua
+++ media/lua/client/Boats/Swim.lua
@@ -1,383 +1,85 @@
-require("Boats/Init")
-print("AUQATSAR LOADED: Swim.lua")
-AquatsarYachts.Swim = {}
 
-function AquatsarYachts.Swim.isWater(square)
-	return square ~= nil and square:Is(IsoFlagType.water)
+local function swimToBoatPerform(playerObj, boat)
+
 end
 
-function AquatsarYachts.Swim.chanceSuccess(playerObj, square)
-    local x = playerObj:getX() - square:getX()
-    local y = playerObj:getY() - square:getY()    
-    local dist = math.sqrt(x*x + y*y)
-    local canSwim = playerObj:isRecipeKnown("Swimming")
-    local haveLifebouy = playerObj:getInventory():containsType("Lifebuoy")
+local function swimToPointPerform(playerObj, square)
 
-    local chance
-    if dist < 10 then
-        if canSwim or haveLifebouy then 
-            chance = 100
-        else
-            chance = 80
-        end
-    elseif dist < 30 then
-        if canSwim or haveLifebouy then 
-            chance = 95
-        else
-            chance = 60
-        end
-    else
-        if canSwim or haveLifebouy then 
-            chance = 80
-        else
-            chance = 20
-        end
-    end
-
-    local haveDivingMask = playerObj:getInventory():containsType("DivingMask")
-    if haveDivingMask then 
-        chance = chance * 1.1
-    end
-
-    local equipWeight = round(playerObj:getInventory():getCapacityWeight(), 2)    
-    chance = chance * (1 - (equipWeight/40))
-    
-    local Unlucky = playerObj:HasTrait("Unlucky")
-    if Unlucky then 
-        chance = chance * 0.8
-    end
-
-    local Lucky = playerObj:HasTrait("Lucky")
-    if Lucky then 
-        chance = chance * 1.2
-    end
-    
-    local Overweight = playerObj:HasTrait("Overweight")
-    if Overweight then 
-        chance = chance * 0.9
-    end
-    
-    local Obese = playerObj:HasTrait("Obese")
-    if Obese then 
-        chance = chance * 0.8
-    end
-    
-    local panic = playerObj:getMoodles():getMoodleLevel(MoodleType.Panic)
-    chance = chance * (1 - panic/20)
-
-    local drunk = playerObj:getMoodles():getMoodleLevel(MoodleType.Drunk)
-    chance = chance * (1 - drunk/20)
-
-    local endurance = playerObj:getMoodles():getMoodleLevel(MoodleType.Endurance)
-    chance = chance * (1 - endurance/20)
-
-    local tired = playerObj:getMoodles():getMoodleLevel(MoodleType.Tired)
-    chance = chance * (1 - tired/20)
-
-    local pain = playerObj:getMoodles():getMoodleLevel(MoodleType.Pain)
-    chance = chance * (1 - pain/20)
-
-    local Fitness = playerObj:getPerkLevel(Perks.Fitness)
-    chance = chance * (Fitness/10 + 0.5)
-
-    if chance > 100 then 
-        chance = 100
-    elseif chance < 0 then
-        chance = 0
-    end
-
-    return math.floor(chance)
 end
 
-local function compare(a,b)
-    return a:getWeight() > b:getWeight()
+local function swimToLandPerform(playerObj, square)
+
 end
 
-function AquatsarYachts.Swim.dropItems(playerObj)
-    local inv = playerObj:getInventory()    
-    local items = {}
+local function isWaterLine(x, y, x2, y2)  
+    local tmpX = x2 - x
+    local tmpY = y2 - y    
+    local len = math.sqrt(tmpX*tmpX + tmpY*tmpY)
+    local dx = tmpX / len
+    local dy = tmpY / len
 
-    for j=1, inv:getItems():size() do
-        local item = inv:getItems():get(j-1);
-        table.insert(items, item)
-    end
-
-    local dropNum = ZombRand(#items * 0.6)
-    table.sort(items, compare)
-
-    for i=1, dropNum do
-        inv:DoRemoveItem(items[i])
-    end
-end
-
-function AquatsarYachts.Swim.swimToLand(playerObj, square, chance)
-    local equipWeight = round(playerObj:getInventory():getCapacityWeight(), 2)
-    local isFail = ZombRand(100) > chance
-
-    if equipWeight > 20 then
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.Torso_Upper):AddDamage(ZombRand(30))
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.Torso_Lower):AddDamage(ZombRand(30))
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperLeg_L):AddDamage(ZombRand(30))
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperLeg_R):AddDamage(ZombRand(30))
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperArm_L):AddDamage(ZombRand(30))
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperArm_R):AddDamage(ZombRand(30))    
-        AquatsarYachts.Swim.dropItems(playerObj)
-
-        local endurance = playerObj:getStats():getEndurance() - 0.5
-        if endurance < 0 then endurance = 0 end
-        playerObj:getStats():setEndurance(endurance);
-        playerObj:getBodyDamage():setWetness(100);
-
-        playerObj:Say(getText("IGUI_almostDieFail"))
-        playerObj:Say(getText("IGUI_andDropItems"))
-    else
-        if isFail then
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.Torso_Upper):AddDamage(ZombRand(30))
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.Torso_Lower):AddDamage(ZombRand(30))
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperLeg_L):AddDamage(ZombRand(30))
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperLeg_R):AddDamage(ZombRand(30))
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperArm_L):AddDamage(ZombRand(30))
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperArm_R):AddDamage(ZombRand(30))    
-            AquatsarYachts.Swim.dropItems(playerObj)   
-            
-            playerObj:Say(getText("IGUI_almostDie"))
-            playerObj:Say(getText("IGUI_andDropItems"))
-        end
-
-        local vehicle = playerObj:getVehicle()
-        local seat = vehicle:getSeat(playerObj)
-        vehicle:exit(playerObj)
-        playerObj:PlayAnim("Idle")
-        triggerEvent("OnExitVehicle", playerObj)
-        vehicle:updateHasExtendOffsetForExitEnd(playerObj)
-        playerObj:setX(square:getX())
-        playerObj:setY(square:getY())
-
-        local endurance = playerObj:getStats():getEndurance() - 0.5
-        if endurance < 0 then endurance = 0 end
-        playerObj:getStats():setEndurance(endurance);
-        playerObj:getBodyDamage():setWetness(100);
-    end
-end
-
-function AquatsarYachts.Swim.getSwimSquares(x, y)
-    local squares = {}
     local cell = getCell()
-
-    local NminDist = 50
-    local SminDist = 50
-    local EminDist = 50
-    local WminDist = 50
-
-
-    for i=1, 50 do
-        for j = -i, i do
-            local sq = cell:getGridSquare(x+i, y+j, 0)
-            if not AquatsarYachts.Swim.isWater(sq) and sq:isNotBlocked(true) then
-                local dist = math.sqrt((i*i + j*j))
-
-                if dist < EminDist then 
-                    EminDist = dist
-                    squares["EAST"] = sq
-                end
-            end
-            
-        end 
-    end
-
-    for i=1, 50 do
-        for j = -i, i do
-            local sq = cell:getGridSquare(x-i, y+j, 0)
-            if not AquatsarYachts.Swim.isWater(sq) and sq:isNotBlocked(true) then
-                local dist = math.sqrt((i*i + j*j))
-
-                if dist < WminDist then 
-                    WminDist = dist
-                    squares["WEST"] = sq
-                end
-            end
-            
-        end 
-    end
-
-    for i=1, 50 do
-        for j = -i, i do
-            local sq = cell:getGridSquare(x+j, y+i, 0)
-            
-            if not AquatsarYachts.Swim.isWater(sq) and sq:isNotBlocked(true) then
-                local dist = math.sqrt((i*i + j*j))
-
-                if dist < SminDist then 
-                    SminDist = dist
-                    squares["SOUTH"] = sq
-                end
-            end
-        end 
-    end
-
-    for i=1, 50 do
-        for j = -i, i do
-            local sq = cell:getGridSquare(x+j, y-i, 0)
-            
-            if not AquatsarYachts.Swim.isWater(sq) and sq:isNotBlocked(true) then
-                local dist = math.sqrt((i*i + j*j))
-
-                if dist < NminDist then 
-                    NminDist = dist
-                    squares["NORTH"] = sq
-                end
-            end
-        end 
-    end
-
-
-    return squares
-end
-
---------------------------
-local function startSwimToBoat(playerObj, boat, chance)
-    local equipWeight = round(playerObj:getInventory():getCapacityWeight(), 2)
-    local isFail = ZombRand(100) > chance
-
-    if equipWeight > 20 then
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.Torso_Upper):AddDamage(ZombRand(30))
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.Torso_Lower):AddDamage(ZombRand(30))
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperLeg_L):AddDamage(ZombRand(30))
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperLeg_R):AddDamage(ZombRand(30))
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperArm_L):AddDamage(ZombRand(30))
-        playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperArm_R):AddDamage(ZombRand(30))    
-        AquatsarYachts.Swim.dropItems(playerObj)
-
-        local endurance = playerObj:getStats():getEndurance() - 0.5
-        if endurance < 0 then endurance = 0 end
-        playerObj:getStats():setEndurance(endurance);
-        playerObj:getBodyDamage():setWetness(100);
-
-        playerObj:Say(getText("IGUI_almostDieFail"))
-        playerObj:Say(getText("IGUI_andDropItems"))
-    else
-        if isFail then
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.Torso_Upper):AddDamage(ZombRand(30))
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.Torso_Lower):AddDamage(ZombRand(30))
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperLeg_L):AddDamage(ZombRand(30))
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperLeg_R):AddDamage(ZombRand(30))
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperArm_L):AddDamage(ZombRand(30))
-            playerObj:getBodyDamage():getBodyPart(BodyPartType.UpperArm_R):AddDamage(ZombRand(30))    
-            AquatsarYachts.Swim.dropItems(playerObj)
-
-            playerObj:Say(getText("IGUI_almostDie"))
-            playerObj:Say(getText("IGUI_andDropItems"))
-        end
-
-        boat:enter(0, playerObj)
-        boat:setCharacterPosition(playerObj, 0, "inside")
-        boat:transmitCharacterPosition(0, "inside")
-        boat:playPassengerAnim(0, "idle")
-        triggerEvent("OnEnterVehicle", playerObj)
-
-        local endurance = playerObj:getStats():getEndurance() - 0.5
-        if endurance < 0 then endurance = 0 end
-        playerObj:getStats():setEndurance(endurance);
-        playerObj:getBodyDamage():setWetness(100);
-    end
-end
-
-local function getBoatsForSwimTo(square)
-    local cell = getCell()
-    local x = square:getX()
-    local y = square:getY()
-    
-    local boats = {}
-
-    for i=-50, 50 do
-        for j =-50, 50 do
-            local sq = cell:getGridSquare(x+i, y+j, 0)
-            
-            if AquatsarYachts.Swim.isWater(sq) then
-                for i=1, sq:getMovingObjects():size() do
-					local obj = sq:getMovingObjects():get(i-1)
-					if obj~= nil and instanceof(obj, "BaseVehicle") and AquaConfig.isBoat(obj) then
-						boats[obj] = true
-					end
-				end
-            end
-        end 
-    end
-
-    local answer = {}
-    for key, value in pairs(boats) do
-        table.insert( answer, key)
-    end
-
-    return answer
-end
-
-
-local function getAnotherLandSquare(playerObj, square)
-    local x = square:getX() - playerObj:getX()
-    local y = square:getY() - playerObj:getY()
-    local len = math.sqrt(x*x + y*y)
-    
-    local dx = x / len
-    local dy = y / len
-    x = square:getX()
-    y = square:getY()
-    local cell = getCell()
-
-    local lastWaterSq
-    for i=1, (250 - math.floor(len)) do 
+    for i=1, math.floor(len) do
         local sq = cell:getGridSquare(x + dx*i, y + dy*i, 0)
-        if sq and not WaterNWindPhysics.isWater(sq) and sq:isNotBlocked(true) then
-            return sq
-        end
-        if sq then
-            lastWaterSq = sq
-        end
+        if not sq or not sq:Is(IsoFlagType.water) then 
+            return false
+        end    
     end
 
-    return lastWaterSq
+    return true
 end
 
+local function getLineNearestLand(x, y, x2, y2)  
+    local tmpX = x2 - x
+    local tmpY = y2 - y    
+    local len = math.sqrt(tmpX*tmpX + tmpY*tmpY)
+    local dx = tmpX / len
+    local dy = tmpY / len
 
-local function startSwimToAnotherLand(playerObj, square)
-    playerObj:setX(square:getX())
-    playerObj:setY(square:getY())
+    local cell = getCell()
+    for i=1, math.floor(len) do
+        local sq = cell:getGridSquare(x + dx*i, y + dy*i, 0)
+        if not sq or not sq:Is(IsoFlagType.water) then 
+            return sq
+        end    
+    end
 end
 
-
-local function swimToBoat(player, context, worldobjects, test)
-    local waterSquare = nil
+local function swimToPoint(player, context, worldobjects, test)
     local playerObj = getSpecificPlayer(player)
-
+    local playerSquare = playerObj:getSquare()
+    local pointSquare
+    local boat
+    
     if playerObj:getVehicle() then return end
 
 	for i,v in ipairs(worldobjects) do
-		square = v:getSquare();
-        if square and square:Is(IsoFlagType.water) then
-            waterSquare = square
-            break
+		local square = v:getSquare();
+        
+        if square then
+            pointSquare = square
+
+            local vehicle = square:getVehicleContainer()
+            if vehicle and AquaConfig.isBoat(vehicle) then
+                boat = vehicle
+            end
         end
     end
     
-    if waterSquare then
-        local swimToBoatOption = context:addOption(getText("IGUI_SwimTo"), nil, nil)
-        local subMenuSwimToBoat = context:getNew(context)
-        context:addSubMenu(swimToBoatOption, subMenuSwimToBoat)
-        
-        local boats = getBoatsForSwimTo(waterSquare)
-        for i=1, #boats do
-            local name = getText("IGUI_BoatName" .. boats[i]:getScript():getName())
-            local chance = AquatsarYachts.Swim.chanceSuccess(playerObj, boats[i]:getSquare())
-            subMenuSwimToBoat:addOption(name .. " (" .. getText("IGUI_chance") .. ": " .. chance .. "%)", playerObj, startSwimToBoat, boats[i], chance)
-        end
+    if boat then
+        context:addOption(getText("IGUI_SwimToBoat"), playerObj, swimToBoatPerform, boat)
+    
+    elseif pointSquare and isWaterLine(playerObj:getX(), playerObj:getY(), pointSquare:getX(), pointSquare:getY()) then
+        context:addOption(getText("IGUI_SwimToPoint"), playerObj, swimToPointPerform, pointSquare)
 
-        local sq = getAnotherLandSquare(playerObj, waterSquare)
-        if sq ~= nil then
-            local chance = AquatsarYachts.Swim.chanceSuccess(playerObj, sq)
-            subMenuSwimToBoat:addOption(getText("IGUI_AnotherLand").. " (" .. getText("IGUI_chance") .. ": " .. chance .. "%)", playerObj, startSwimToAnotherLand, sq)
-        end
+    elseif pointSquare and playerSquare and playerSquare:Is(IsoFlagType.water) then
+        if not pointSquare:Is(IsoFlagType.water) then
+            local sq = getLineNearestLand(playerObj:getX(), playerObj:getY(), pointSquare:getX(), pointSquare:getY())
+            if sq then 
+                context:addOption(getText("IGUI_SwimToLand"), playerObj, swimToLandPerform, sq)
+            end
+        end        
     end
-
 end
 
-Events.OnFillWorldObjectContextMenu.Add(swimToBoat);
+Events.OnFillWorldObjectContextMenu.Add(swimToPoint)
