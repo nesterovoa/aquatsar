a7385dd0ca716bae82ce7195d9952fc1bc8cce00
Alexander Blinov <sashablinov1998@gmail.com>
2021-01-12 22:36:04 +0500

    Spawn aqua trailers

---
+++ media/lua/shared/AquaTrailerSpawnList.lua
@@ -0,0 +1,13 @@
+if VehicleZoneDistribution then
+
+    VehicleZoneDistribution.parkingstall.vehicles["Base.TrailerWithBoatSailingYacht"] = {index = -1, spawnChance = 1};
+
+    VehicleZoneDistribution.trailerpark.vehicles["Base.TrailerWithBoatSailingYacht"] = {index = -1, spawnChance = 2};
+
+    VehicleZoneDistribution.bad.vehicles["Base.TrailerWithBoatSailingYacht"] = {index = -1, spawnChance = 2};
+
+    VehicleZoneDistribution.medium.vehicles["Base.TrailerWithBoatSailingYacht"] = {index = -1, spawnChance = 1};
+
+    VehicleZoneDistribution.good.vehicles["Base.TrailerWithBoatSailingYacht"] = {index = -1, spawnChance = 1};
+
+end
