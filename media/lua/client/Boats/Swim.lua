8813f93cb242296b76f9b59f53e05a69d693b99f
Alexander Blinov <sashablinov1998@gmail.com>
2021-01-12 19:47:36 +0500

    Fixed bug - swim on ground to boat

--- media/lua/client/Boats/Swim.lua
+++ media/lua/client/Boats/Swim.lua
@@ -175,7 +175,7 @@ local function swimToPoint(player, context, worldobjects, test)
         end
     end
     
-    if boat then
+    if boat and pointSquare and pointSquare ~= playerSquare and isWaterLine(playerObj:getX(), playerObj:getY(), pointSquare:getX(), pointSquare:getY()) then
         local chance = AquatsarYachts.Swim.swimChanceSuccess(playerObj, boat:getSquare())
         local name = getText("IGUI_BoatName" .. boat:getScript():getName())
         context:addOption(getText("IGUI_SwimTo", name).. " (" .. getText("IGUI_chance") .. ": " .. chance .. "%)", playerObj, swimToBoatPerform, boat, chance)
