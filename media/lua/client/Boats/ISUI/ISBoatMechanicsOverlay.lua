e59207ab195c0a39c217a967406062be7c5c8c00
iBrRus <v.i.kolesnikov@hotmail.com>
2021-01-14 19:34:05 +0300

    Add mechanic UI for Sailing Yacht with Sails.

--- media/lua/client/Boats/ISUI/ISBoatMechanicsOverlay.lua
+++ media/lua/client/Boats/ISUI/ISBoatMechanicsOverlay.lua
@@ -16,41 +16,57 @@
 ISBoatMechanicsOverlay = {};
 ISBoatMechanicsOverlay.BoatList = {};
 ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"] = {imgPrefix = "salingboat_", x=10,y=-30};
-ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYachtWithSailsLeft"] = {imgPrefix = "salingboat_", x=10,y=-30};
-ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYachtWithSailsRight"] = {imgPrefix = "salingboat_", x=10,y=-30};
+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYachtWithSailsLeft"] = {imgPrefix = "salingboatWS_", x=10,y=-30};
+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYachtWithSailsRight"] = {imgPrefix = "salingboatWS_", x=10,y=-30};
 
 ISBoatMechanicsOverlay.PartList = {};
+ISBoatMechanicsOverlay.PartList["Sails"] = {img="sails", vehicles={}};
+ISBoatMechanicsOverlay.PartList["Sails"].vehicles["salingboatWS_"] = {x=72+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=93+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=131+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=260+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+
+
 ISBoatMechanicsOverlay.PartList["Battery"] = {img="battery", vehicles={}};
 ISBoatMechanicsOverlay.PartList["Battery"].vehicles["salingboat_"] = {x=4+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=373+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=47+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=403+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
-
+ISBoatMechanicsOverlay.PartList["Battery"].vehicles["salingboatWS_"] = {x=4+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=373+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=47+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=403+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
 
 ISBoatMechanicsOverlay.PartList["Engine"] = {img="engine", vehicles = {}};
 ISBoatMechanicsOverlay.PartList["Engine"].vehicles["salingboat_"] = {x=5+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=500+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=86+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=550+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+ISBoatMechanicsOverlay.PartList["Engine"].vehicles["salingboatWS_"] = {x=5+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=500+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=86+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=550+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
 
 ISBoatMechanicsOverlay.PartList["GasTank"] = {img="gastank", vehicles = {}};
 ISBoatMechanicsOverlay.PartList["GasTank"].vehicles["salingboat_"] = {x=146+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=500+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=255+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=550+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+ISBoatMechanicsOverlay.PartList["GasTank"] = {img="gastank", vehicles = {}};
+ISBoatMechanicsOverlay.PartList["GasTank"].vehicles["salingboatWS_"] = {x=146+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=500+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=255+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=550+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
 
 ISBoatMechanicsOverlay.PartList["LightFloodlightLeft"] = {img="headlight_left", vehicles = {}};
 ISBoatMechanicsOverlay.PartList["LightFloodlightLeft"].vehicles["salingboat_"] = {x=103+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=260+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=112+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=277+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+ISBoatMechanicsOverlay.PartList["LightFloodlightLeft"].vehicles["salingboatWS_"] = {x=103+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=260+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=112+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=277+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
 
 ISBoatMechanicsOverlay.PartList["LightFloodlightRight"] = {img="headlight_right", vehicles = {}};
 ISBoatMechanicsOverlay.PartList["LightFloodlightRight"].vehicles["salingboat_"] = {x=150+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=260+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=159+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=277+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+ISBoatMechanicsOverlay.PartList["LightFloodlightRight"].vehicles["salingboatWS_"] = {x=150+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=260+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=159+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=277+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
 
 ISBoatMechanicsOverlay.PartList["WindowFrontLeft"] = {img="window_front_left", vehicles = {}};
 ISBoatMechanicsOverlay.PartList["WindowFrontLeft"].vehicles["salingboat_"] = {x=84+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=289+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=92+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=304+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+ISBoatMechanicsOverlay.PartList["WindowFrontLeft"].vehicles["salingboatWS_"] = {x=84+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=289+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=92+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=304+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
 
 ISBoatMechanicsOverlay.PartList["WindowFrontRight"] = {img="window_front_right", vehicles = {}};
 ISBoatMechanicsOverlay.PartList["WindowFrontRight"].vehicles["salingboat_"] = {x=169+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=289+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=178+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=304+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+ISBoatMechanicsOverlay.PartList["WindowFrontRight"].vehicles["salingboatWS_"] = {x=169+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=289+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=178+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=304+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
 
 ISBoatMechanicsOverlay.PartList["WindowRearLeft"] = {img="window_rear_left", vehicles = {}};
 ISBoatMechanicsOverlay.PartList["WindowRearLeft"].vehicles["salingboat_"] = {x=79+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=320+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=87+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=340+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+ISBoatMechanicsOverlay.PartList["WindowRearLeft"].vehicles["salingboatWS_"] = {x=79+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=320+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=87+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=340+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
 
 ISBoatMechanicsOverlay.PartList["WindowRearRight"] = {img="window_rear_right", vehicles = {}};
 ISBoatMechanicsOverlay.PartList["WindowRearRight"].vehicles["salingboat_"] = {x=174+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=320+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=182+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=340+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+ISBoatMechanicsOverlay.PartList["WindowRearRight"].vehicles["salingboatWS_"] = {x=174+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=320+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=182+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=340+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
 
 ISBoatMechanicsOverlay.PartList["Windshield"] = {img="window_windshield", vehicles = {}};
 ISBoatMechanicsOverlay.PartList["Windshield"].vehicles["salingboat_"] = {x=120+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=214+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=140+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=237+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+ISBoatMechanicsOverlay.PartList["Windshield"].vehicles["salingboatWS_"] = {x=132+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=214+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=140+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=237+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
 
 ISBoatMechanicsOverlay.PartList["Propeller"] = {img="propeller", vehicles = {}};
 ISBoatMechanicsOverlay.PartList["Propeller"].vehicles["salingboat_"] = {x=106+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=538+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=157+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=589+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+ISBoatMechanicsOverlay.PartList["Propeller"].vehicles["salingboatWS_"] = {x=106+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y=538+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y,x2=157+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].x,y2=589+ISBoatMechanicsOverlay.BoatList["Base.BoatSailingYacht"].y};
+
 
