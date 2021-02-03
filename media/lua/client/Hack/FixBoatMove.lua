7771934e579e6a8f01dbd97daf7ffb205690a218
Alexander Blinov <sashablinov1998@gmail.com>
2021-02-03 19:25:08 +0500

    Remove old aud fix boat move code

--- media/lua/client/Hack/FixBoatMove.lua
+++
@@ -1,8 +0,0 @@
-local function fixBoatMove()
-    local veh = getPlayer():getVehicle()
-    local vec = veh:getScript():getCenterOfMassOffset()
-    vec:set(vec:x()+0.1, vec:y(), vec:z())
-    veh:scriptReloaded()
-end
-
-AUD.setButton(1, "Fix Boat", fixBoatMove)
